d0c8681300bba7090e88fff067e8596fef7d7725
mahadihasan2022 <mahadihasanmim72@gmail.com>
2022-03-02 04:11:07 +0600

    my commit

--- phone-bazar.js
+++ phone-bazar.js
@@ -1,20 +1,42 @@
+// search system
 const searchPhone = () => {
-  const searchField = document.getElementById("search-area");
-  const searchText = searchField.value;
-//   console.log(searchText);
-  searchField.value = "";
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-//   console.log(url);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => displaySearchResult(data.meals));
-}
+  const searchfield = document.getElementById("search-area");
+  const searchtext = searchfield.value;
+  // console.log(searchtext);
 
-const displaySearchResult = meals =>{
-// console.log(meals);
-const searchResult = document.getElementById('search-result');
-meals.forEach(meal => {
-    // console.log(meal);
-});
-    
+  searchfield.value = "";
+
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchtext}`;
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((status) => displaySearchResult(status.data));
+};
+
+const displaySearchResult = (data) => {
+  // console.log(data);
+  const searchResult = document.getElementById("search-result");
+  data.forEach((data) => {
+    console.log(data);
+
+    const div = document.createElement("div");
+    div.classList.add("col");
+
+    div.innerHTML = `
+          <div onclick="loadPhoneDetail(${data.iddata})" class="card h-100">
+              <img src="${data.image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${data.phone_name}</h5>
+                  <p class="card-text">${data.brand}</p>
+                  <p class="card-slug">${data.slug}</p>
+                </div>
+          </div> 
+      `;
+
+    searchResult.appendChild(div);
+  });
+};
+
+const loadPhoneDetail = (dataSlug) => {
+  console.log(dataSlug);
 }
